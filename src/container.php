53949f653ae4c243e97ba0c87be847980c12360b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-11-10 17:01:36 +0100

    Add `wx_container*` functions

--- src/container.php
+++ src/container.php
@@ -14,7 +14,7 @@ use Isotop\Wx\Container;
  * @return mixed
  */
 function wx_get_once( $key, $fn = null, $default = null ) {
-	$container = Container::instance();
+	$container = wx_container();
 
 	// Check if key exists in the container and return value if so.
 	if ( $container->bound( $key ) ) {
@@ -35,4 +35,65 @@ function wx_get_once( $key, $fn = null, $default = null ) {
 	}
 
 	return $container->bind( $key, $value );
+}
+
+/**
+ * Get the container instance.
+ *
+ * @return \Isotop\Wx\Container
+ */
+function wx_container() {
+	return Container::instance();
+}
+
+/**
+ * Determine if a key exists in the container or not.
+ *
+ * @param  string $key
+ *
+ * @return bool
+ */
+function wx_container_exists( $key ) {
+	return wx_container()->bound( $key );
+}
+
+/**
+ * Get value from container by key.
+ *
+ * @param  string $key
+ * @param  array  $parameters
+ * @param  mixed  $default
+ *
+ * @return mixed
+ */
+function wx_container_get( $key, array $parameters = [], $default = null ) {
+	$container = wx_container();
+
+	if ( $container->bound( $key ) ) {
+		return $container->make( $key, $parameters );
+	}
+
+	return $default;
+}
+
+/**
+ * Set value in container by key.
+ *
+ * @param  string $key
+ * @param  mixed  $default
+ * @param  bool   $singleton
+ *
+ * @return mixed
+ */
+function wx_container_set( $key, $value, $singleton = false ) {
+	return wx_container()->bind( $key, $value, $singleton );
+}
+
+/**
+ * Delete value in container by key.
+ *
+ * @param  string $key
+ */
+function wx_container_delete( $key ) {
+	wx_container()->remove( $key );
 }
--- tests/class-container-test.php
+++ tests/class-container-test.php
@@ -6,7 +6,7 @@ use Isotop\Wx\Container;
 
 class Container_Test extends \WP_UnitTestCase {
 
-	public function test_wp_get_once() {
+	public function test_wx_get_once() {
 		$key = __FUNCTION__;
 
 		$value = wx_get_once( $key, function () {
@@ -23,4 +23,38 @@ class Container_Test extends \WP_UnitTestCase {
 		$this->assertSame( 'hello', $value );
 	}
 
+	public function test_wx_container_exists() {
+		$key = __FUNCTION__;
+		$this->assertFalse( wx_container_exists( $key ) );
+
+		wx_container_set( $key, 'hello' );
+		$this->assertTrue( wx_container_exists( $key ) );
+	}
+
+	public function test_wx_container_get() {
+		$key = __FUNCTION__;
+		$this->assertNull( wx_container_get( $key ) );
+
+		wx_container_set( $key, 'hello' );
+		$this->assertSame( 'hello', wx_container_get( $key ) );
+	}
+
+	public function test_wx_container_set() {
+		$key = __FUNCTION__;
+		$this->assertNull( wx_container_get( $key ) );
+
+		wx_container_set( $key, 'hello' );
+		$this->assertSame( 'hello', wx_container_get( $key ) );
+	}
+
+	public function test_wx_container_delete() {
+		$key = __FUNCTION__;
+		$this->assertNull( wx_container_get( $key ) );
+
+		wx_container_set( $key, 'hello' );
+		$this->assertSame( 'hello', wx_container_get( $key ) );
+
+		wx_container_delete( $key );
+		$this->assertNull( wx_container_get( $key ) );
+	}
 }
